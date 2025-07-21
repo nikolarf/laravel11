89c95f8571f5abdfd56043330bc1db3b5400c3c6
nikolarf <nikolaraf@hotmail.com>
2025-07-21 11:56:08 +0200

    Laravel 11 11

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employer extends Model
 {
-    /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
 }
